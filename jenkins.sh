c4989921b5c0919389945f6223488852480520da
jenkinshrg <jenkinshrg@gmail.com>
2016-01-22 16:10:32 +0900

    modify script

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,9 @@ cp config.sh.sample config.sh
 sed -i -e "s/HOME/WORKSPACE/g" config.sh
 
 bash -xe ./getsource.sh
+sed -i -e 's/apt-get /apt-get -y /g' ${WORKSPACE}/src/openhrp3/util/installPackages.sh
+sed -i -e 's/exit 1/exit 0/g' ${WORKSPACE}/src/openhrp3/util/installPackages.sh
+sed -i -e 's/apt-get /apt-get -y /g' ${WORKSPACE}/src/choreonoid/misc/script/install-requisites-ubuntu-14.04.sh
 
 bash -xe ./setupenv.sh
 
