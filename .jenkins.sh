989ea3cac996b78383c284088a96280b8044e99d
jenkinshrg <jenkinshrg@gmail.com>
2016-02-16 13:12:36 +0900

    modify script

--- .jenkins.sh
+++ .jenkins.sh
@@ -26,7 +26,7 @@ source config.sh
 
 if [ ! -e $SRC_DIR ]; then
     mkdir $SRC_DIR
-    sudo apt-get -y install wget ca-certificates openssh-client
+    sudo apt-get -y install wget ca-certificates
     bash -e ./getsource.sh
     if [ "$INTERNAL_MACHINE" -eq 0 ]; then
     sed -i -e 's/apt-get /apt-get -y /g' $SRC_DIR/openhrp3/util/installPackages.sh
