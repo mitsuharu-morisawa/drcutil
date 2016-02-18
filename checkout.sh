3c1b7dbfe927a98c0bce59a311f76736a2e8d9f2
jenkinshrg <jenkinshrg@gmail.com>
2016-02-18 10:39:40 +0900

    modify script

--- checkout.sh
+++ checkout.sh
@@ -13,21 +13,26 @@ cd $SRC_DIR
 
 pull_source openhrp3 hrpsys-base hmc2 hrpsys-humanoid
 
+echo HRP2
 cd HRP2
 git pull
 cd ..
 
+echo HRP2KAI
 cd HRP2KAI
 git pull
 cd ..
 
+echo hrpsys-private
 cd hrpsys-private
 git pull
 cd ..
 
 if [ "$INTERNAL_MACHINE" -eq 0 ]; then
+echo choreonoid
 cd choreonoid
 GIT_SSL_NO_VERIFY=1 git pull
+echo choreonoid/ext/hrpcnoid
 cd ext/hrpcnoid
 git pull
 cd ../../..
