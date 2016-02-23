e460209515f53c36370c7662b72deecbf0909c89
jenkinshrg <jenkinshrg@gmail.com>
2016-02-23 15:08:26 +0900

    modify script

--- diff.sh
+++ diff.sh
@@ -41,6 +41,26 @@ fetch_log_nolink() {
     done
 }
 
+fetch_log_nolink_noverify() {
+    for dir_name in $@; do
+        cd "$dir_name"
+        echo -n > $SRC_DIR/${dir_name}.diff
+        URL=$(git config --get remote.origin.url)
+        LOCAL_ID=$(git log -1 HEAD --pretty=format:"%H")
+        GIT_SSL_NO_VERIFY=1 git fetch
+        git log origin HEAD --pretty=format:"%H,%h" | while read line
+        do
+            REMOTE_ID=$(echo "${line}" | cut -d "," -f 1)
+            if [ $REMOTE_ID == $LOCAL_ID ]; then
+                break
+            fi
+            SHORT_ID=$(echo "${line}" | cut -d "," -f 2)
+            echo "${dir_name},$SHORT_ID," >> $SRC_DIR/${dir_name}.diff
+        done
+        cd ..
+    done
+}
+
 fetch_log "openhrp3" "hrpsys-base"
 
 if [ "$HAVE_ATOM_ACCESS" -eq 1 ]; then
@@ -54,7 +74,7 @@ if [ "$HAVE_ATOM_ACCESS" -eq 1 ]; then
 fi
 
 if [ "$INTERNAL_MACHINE" -eq 0 ]; then
-fetch_log_nolink "choreonoid"
+fetch_log_nolink_noverify "choreonoid"
 
 cd choreonoid/ext
 fetch_log "hrpcnoid"
