2cad5efc3e24e6791d8adde696863059fe019156
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-05-06 10:18:39 +0900

    updates summary

--- .jenkins/printJenkinsResultSummary.py
+++ .jenkins/printJenkinsResultSummary.py
@@ -4,13 +4,40 @@
 import sys, urllib2, json
 from datetime import datetime
 
+def printLatestResults(url, job, n):
+    print "|[" + job + "](http://jenkinshrg.github.io#" + job + ")|![Build Status](http://jenkinshrg.github.io/"+ job + ".svg)|",
+    try:
+        url = url + 'job/' + job + '/api/json?tree=builds[building,duration,number,result,timestamp,url]'
+        r = urllib2.urlopen(url)
+        root = json.loads(r.read())
+        builds = root['builds']
+    except:
+        print "|"*n
+    finally:
+        r.close()
+    for i in range(n):
+        if i < len(builds):
+            result = builds[i]['result']
+            if result == "SUCCESS":
+                color = "blue"
+            elif result == "UNSTABLE":
+                color = "yellow"
+            elif result == "FAILURE":
+                color = "red"
+            else:
+                color = "aborted"
+            print "![Jenkins Icon](http://jenkinshrg.github.io/images/24x24/"+ color + ".png)",
+        print "|",
+    print 
+
+
 if len(sys.argv) > 1:
-    url = sys.argv[1]
+    topurl = sys.argv[1]
 else:
-    url = "http://localhost:8080/"
+    topurl = "http://localhost:8080/"
 
 try:
-    url = url + 'api/json?tree=jobs[name,lastStableBuild]'
+    url = topurl + 'api/json?tree=jobs[name,lastStableBuild]'
     r = urllib2.urlopen(url)
     root = json.loads(r.read())
     jobs = root['jobs']
@@ -18,6 +45,8 @@ except:
     sys.exit(1)
 finally:
     r.close()
+
+njob = 10
 
 print "---"
 print "layout: default"
@@ -28,9 +57,9 @@ print ""
 print "### Job Summary"
 print "___"
 print ""
-print "|Name|Status|"
-print "|---|---|"
+print "|Name|Status|Latest Results"+"|"*njob
+print "|---|---|"+"--|"*njob
 for job in jobs:
-    if job['name'] == "drcutil-build-32" or job['name'] == "drcutil-build-64" or job['name'] == "drcutil-task-balancebeam" or job['name'] == "drcutil-task-terrain" or job['name'] == "drcutil-task-valve" or job['name'] == "drcutil-task-wall" or job['name'] == "hrp5p-task-terrain" or job['name'] == "hrp2kai-task-door":
-        print "|[" + job['name'] + "](http://jenkinshrg.github.io#" + job['name'] + ")|![Build Status](http://jenkinshrg.github.io/"+ job['name'] + ".svg)|"
+    if job['name'] != "drcutil" and job['name'] != "drcutil-upload":
+        printLatestResults(topurl, job['name'], njob)
 print ""
