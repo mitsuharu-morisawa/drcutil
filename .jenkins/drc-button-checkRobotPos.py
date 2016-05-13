7ef65ba880fc234fda792390318421419e07f7ab
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-05-13 18:40:23 +0900

    adds drc-button-checkRobotPos.py

---
+++ .jenkins/drc-button-checkRobotPos.py
@@ -0,0 +1,16 @@
+import sys
+
+num = 0
+for line in sys.stdin:
+    num += 1
+    if num == 2:
+        position = eval(line)
+    if num == 3:
+        rotation = eval(line)
+
+if rotation[0] > -0.1 and rotation[0] < 0.1 and \
+   rotation[1] > -0.1 and rotation[1] < 0.1 and \
+   rotation[2] > -2.0 and rotation[2] < 2.0:
+    print "OK"
+else:
+    print "FALL"
