579c68f73d67d2bdfaa97fb6b7da10f507a31c46
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-05-27 19:10:46 +0900

    adds clean.sh

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source config.sh
+
+rm -rf $SRC_DIR/openhrp3/build
+rm -rf $SRC_DIR/HRP2/build
+rm -rf $SRC_DIR/HRP2KAI/build
+rm -rf $SRC_DIR/HRP5P/build
+rm -rf $SRC_DIR/hrpsys-base/build
+rm -rf $SRC_DIR/hrpsys-private/build
+rm -rf $SRC_DIR/hrpsys-humanoid/build
+rm -rf $SRC_DIR/hmc2/build
+rm -rf $SRC_DIR/choreonoid/build
+
+
+
