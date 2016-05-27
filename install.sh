81bc7cf1b4d6e6cf1b64726145348774aeebcd65
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-05-28 08:04:01 +0900

    adds options for onboard computer

--- install.sh
+++ install.sh
@@ -30,12 +30,21 @@ if [ "$UBUNTU_VER" != "16.04" ]; then
    cmake_install_with_option "octomap-1.8.0"
 fi
 
-cmake_install_with_option "hrpsys-base" "-DCOMPILE_JAVA_STUFF=OFF -DBUILD_KALMAN_FILTER=OFF -DBUILD_STABILIZER=OFF -DENABLE_DOXYGEN=OFF"
+if [ "$INTERNAL_MACHINE" -eq 0 ]; then
+    cmake_install_with_option "hrpsys-base" "-DCOMPILE_JAVA_STUFF=OFF -DBUILD_KALMAN_FILTER=OFF -DBUILD_STABILIZER=OFF -DENABLE_DOXYGEN=OFF"
+else
+    cmake_install_with_option "hrpsys-base" "-DCOMPILE_JAVA_STUFF=OFF -DBUILD_KALMAN_FILTER=OFF -DBUILD_STABILIZER=OFF -DENABLE_DOXYGEN=OFF -DINSTALL_HRPIO=OFF"
+fi
 cmake_install_with_option "HRP2" "-DROBOT_NAME=HRP2KAI"
 cmake_install_with_option "HRP2KAI"
 cmake_install_with_option "HRP5P"
-cmake_install_with_option "hmc2" "-DCOMPILE_JAVA_STUFF=OFF"
-cmake_install_with_option "hrpsys-humanoid" "-DCOMPILE_JAVA_STUFF=OFF"
+if [ "$INTERNAL_MACHINE" -eq 0 ]; then
+    cmake_install_with_option "hmc2" "-DCOMPILE_JAVA_STUFF=OFF"
+    cmake_install_with_option "hrpsys-humanoid" "-DCOMPILE_JAVA_STUFF=OFF"
+else
+    cmake_install_with_option "hmc2" "-DCOMPILE_JAVA_STUFF=OFF -DGENERATE_FILES_FOR_SIMULATION=OFF"
+    cmake_install_with_option "hrpsys-humanoid" "-DCOMPILE_JAVA_STUFF=OFF -DGENERATE_FILES_FOR_SIMULATION=OFF"
+fi
 cmake_install_with_option "hrpsys-private"
 if [ "$INTERNAL_MACHINE" -eq 0 ]; then
     cmake_install_with_option "choreonoid" "-DENABLE_CORBA=ON -DBUILD_CORBA_PLUGIN=ON -DBUILD_OPENRTM_PLUGIN=ON -DBUILD_PCL_PLUGIN=ON -DBUILD_OPENHRP_PLUGIN=ON -DBUILD_GRXUI_PLUGIN=ON -DBODY_CUSTOMIZERS=$SRC_DIR/HRP2/customizer/HRP2Customizer;$SRC_DIR/HRP5P/customizer/HRP5PCustomizer -DBUILD_DRC_USER_INTERFACE_PLUGIN=ON"
