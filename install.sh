6e9626ccf9650e0bca1a98c05ac83f567c7b3e24
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-06-17 06:55:02 +0900

    cleans up install.sh

--- install.sh
+++ install.sh
@@ -29,10 +29,11 @@ if [ ! -e configure ]; then
     ./build/autogen
 fi
 if [ $BUILD_TYPE != "Release" ]; then
-    ./configure --prefix=$PREFIX --without-doxygen --enable-debug
+    EXTRA_OPTION="--enable-debug"
 else
-    ./configure --prefix=$PREFIX --without-doxygen
+    EXTRA_OPTION=
 fi
+./configure --prefix=$PREFIX $EXTRA_OPTION
 $SUDO make -j$MAKE_THREADS_NUMBER install
 
 cmake_install_with_option "openhrp3" "-DCOMPILE_JAVA_STUFF=OFF -DBUILD_GOOGLE_TEST=$BUILD_GOOGLE_TEST"
@@ -41,20 +42,21 @@ if [ "$INTERNAL_MACHINE" -eq 0 ]; then
     if [ "$UBUNTU_VER" != "16.04" ]; then
 	cmake_install_with_option "octomap-$OCTOMAP_VERSION"
     fi
-    cmake_install_with_option "hrpsys-base" "-DCOMPILE_JAVA_STUFF=OFF -DBUILD_KALMAN_FILTER=OFF -DBUILD_STABILIZER=OFF -DENABLE_DOXYGEN=OFF"
+    EXTRA_OPTION=
 else
-    cmake_install_with_option "hrpsys-base" "-DCOMPILE_JAVA_STUFF=OFF -DBUILD_KALMAN_FILTER=OFF -DBUILD_STABILIZER=OFF -DENABLE_DOXYGEN=OFF -DINSTALL_HRPIO=OFF"
+    EXTRA_OPTION="-DINSTALL_HRPIO=OFF"
 fi
+cmake_install_with_option "hrpsys-base" "-DCOMPILE_JAVA_STUFF=OFF -DBUILD_KALMAN_FILTER=OFF -DBUILD_STABILIZER=OFF -DENABLE_DOXYGEN=OFF $EXTRA_OPTION"
 cmake_install_with_option "HRP2" "-DROBOT_NAME=HRP2KAI"
 cmake_install_with_option "HRP2KAI"
 cmake_install_with_option "HRP5P"
 if [ "$INTERNAL_MACHINE" -eq 0 ]; then
-    cmake_install_with_option "hmc2" "-DCOMPILE_JAVA_STUFF=OFF"
-    cmake_install_with_option "hrpsys-humanoid" "-DCOMPILE_JAVA_STUFF=OFF"
+    EXTRA_OPTION=
 else
-    cmake_install_with_option "hmc2" "-DCOMPILE_JAVA_STUFF=OFF -DGENERATE_FILES_FOR_SIMULATION=OFF"
-    cmake_install_with_option "hrpsys-humanoid" "-DCOMPILE_JAVA_STUFF=OFF -DGENERATE_FILES_FOR_SIMULATION=OFF"
+    EXTRA_OPTION="-DGENERATE_FILES_FOR_SIMULATION=OFF"
 fi
+cmake_install_with_option "hmc2" "-DCOMPILE_JAVA_STUFF=OFF $EXTRA_OPTION"
+cmake_install_with_option "hrpsys-humanoid" "-DCOMPILE_JAVA_STUFF=OFF $EXTRA_OPTION"
 cmake_install_with_option "hrpsys-private"
 if [ "$INTERNAL_MACHINE" -eq 0 ]; then
     cmake_install_with_option "choreonoid" "-DENABLE_CORBA=ON -DBUILD_CORBA_PLUGIN=ON -DBUILD_OPENRTM_PLUGIN=ON -DBUILD_PCL_PLUGIN=ON -DBUILD_OPENHRP_PLUGIN=ON -DBUILD_GRXUI_PLUGIN=ON -DBODY_CUSTOMIZERS=$SRC_DIR/HRP2/customizer/HRP2Customizer;$SRC_DIR/HRP5P/customizer/HRP5PCustomizer -DBUILD_DRC_USER_INTERFACE_PLUGIN=ON"
