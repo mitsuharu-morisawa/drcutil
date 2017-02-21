76ec53bc1e45c8099127b1a060b6ce4937eb26d6
Jun Inoue <jun.lambda@gmail.com>
2017-02-21 19:09:51 +0900

    Add source-packing

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source config.sh
+source packsrc.sh
 FILENAME="$(echo $(cd $(dirname "$BASH_SOURCE") && pwd -P)/$(basename "$BASH_SOURCE"))"
 RUNNINGSCRIPT="$0"
 trap 'err_report $LINENO $FILENAME $RUNNINGSCRIPT; exit 1' ERR
@@ -28,11 +29,12 @@ build_install() {
 	    if [ "${VERBOSE-0}" -eq 0 ]; then
 		$SUDO make -j$MAKE_THREADS_NUMBER install > $SRC_DIR/${dir_name}.log 2>&1
 	    else
-		$SUDO make -j$MAKE_THREADS_NUMBER install
+		$SUDO make -j$MAKE_THREADS_NUMBER install 2>&1 | tee $SRC_DIR/${dir_name}.log
 	    fi
 	    if [ "$?" -eq 0 ]
 	    then
 		echo "success"
+                built_dirs="$built_dirs $dir_name"
 	    else
 		echo -e "\e[31mfail\e[m"
 	    fi
@@ -41,6 +43,7 @@ build_install() {
     done
 }
 
+built_dirs=
 build_install "openhrp3" "hrpsys-base" "HRP2" "HRP2KAI" "HRP5P" "sch-core" "hmc2" "hrpsys-private" "hrpsys-humanoid" "state-observation" "hrpsys-state-observation"
 
 if [ "$INTERNAL_MACHINE" -eq 0 ]; then
@@ -49,3 +52,5 @@ else
 build_install "flexiport" "hokuyoaist" "rtchokuyoaist"
 fi
 
+packsrc $built_dirs
+cp robot-sources.tar.bz2 $PREFIX/share/
--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 DRCUTIL=$PWD
 
 source config.sh
+source packsrc.sh
 FILENAME="$(echo $(cd $(dirname "$BASH_SOURCE") && pwd -P)/$(basename "$BASH_SOURCE"))"
 RUNNINGSCRIPT="$0"
 trap 'err_report $LINENO $FILENAME $RUNNINGSCRIPT; exit 1' ERR
+built_dirs=
 
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 export PATH=$PREFIX/bin:$PATH
@@ -28,11 +30,13 @@ cmake_install_with_option() {
     cd "$SRC_DIR/$SUBDIR/build"
 
     COMMON_OPTIONS=(-DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "${ASAN_OPTIONS[@]}")
-    echo cmake $(printf "'%s' " "${COMMON_OPTIONS[@]}" "$@") ..
+    echo cmake $(printf "'%s' " "${COMMON_OPTIONS[@]}" "$@") .. | tee config.log
 
-    cmake "${COMMON_OPTIONS[@]}" "$@" ..
+    cmake "${COMMON_OPTIONS[@]}" "$@" .. 2>&1 | tee -a config.log
 
-    $SUDO make -j$MAKE_THREADS_NUMBER install
+    $SUDO make -j$MAKE_THREADS_NUMBER install 2>&1 | tee $SRC_DIR/$SUBDIR.log
+
+    built_dirs="$built_dirs $SUBDIR"
 }
 
 cd $SRC_DIR/OpenRTM-aist
@@ -46,6 +50,8 @@ else
 fi
 ./configure --prefix="$PREFIX" --without-doxygen "${EXTRA_OPTION[@]}"
 
+built_dirs="$built_dirs OpenRTM-aist"
+
 if [ "$ENABLE_ASAN" -eq 1 ]; then
     # We set -fsanitize=address here, after configure, because this
     # flag interferes with detecting the flags needed for pthreads,
@@ -56,7 +62,8 @@ if [ "$ENABLE_ASAN" -eq 1 ]; then
 else
     EXTRA_OPTION=()
 fi
-$SUDO make -j$MAKE_THREADS_NUMBER install "${EXTRA_OPTION[@]}"
+$SUDO make -j$MAKE_THREADS_NUMBER install "${EXTRA_OPTION[@]}" \
+   | tee $SRC_DIR/OpenRTM-aist.log
 
 cmake_install_with_option "openhrp3" -DCOMPILE_JAVA_STUFF=OFF -DBUILD_GOOGLE_TEST="$BUILD_GOOGLE_TEST" -DOPENRTM_DIR="$PREFIX"
 
@@ -111,6 +118,9 @@ else
     cmake_install_with_option hokuyoaist -DBUILD_DOCUMENTATION=OFF -DBUILD_PYTHON_BINDINGS=OFF
     cmake_install_with_option rtchokuyoaist -DBUILD_DOCUMENTATION=OFF
 fi
+
+packsrc $built_dirs
+cp robot-sources.tar.bz2 $PREFIX/share/
 
 echo "add the following line to your .bashrc"
 echo "source $DRCUTIL/setup.bash"
