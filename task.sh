16be2505b2235703a35bcfb8ac5d5a81446a654c
Fumio KANEHIRO <fkanehiro@gmail.com>
2017-03-10 14:35:51 +0900

    use 0 instead of expr

--- task.sh
+++ task.sh
@@ -121,14 +121,14 @@ else
     FREE_AFTER=$(free -m | awk 'NR==2 { print $7 }')
 fi
 if [ "$PS_BEFORE" != "" ] && [ "$PS_AFTER" != "" ]; then
-    PS_CHANGE=$(expr $PS_AFTER - $PS_BEFORE)
+    PS_CHANGE=$(( $PS_AFTER - $PS_BEFORE ))
     echo 'used,change' > $WORKSPACE/choreonoid.csv
     echo $PS_AFTER,$PS_CHANGE >> $WORKSPACE/choreonoid.csv
 fi
 if [ "$FREE_BEFORE" != "" ] && [ "$FREE_AFTER" != "" ]; then
     echo FREE_BEFORE=$FREE_BEFORE
     echo FREE_AFTER=$FREE_AFTER
-    FREE_CHANGE=$(expr $FREE_AFTER - $FREE_BEFORE)
+    FREE_CHANGE=$(( $FREE_AFTER - $FREE_BEFORE ))
     echo 'used,change' > $WORKSPACE/system.csv
     echo $FREE_AFTER,$FREE_CHANGE >> $WORKSPACE/system.csv
 fi
