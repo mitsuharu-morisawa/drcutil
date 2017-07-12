c4c74c2a37b0b02bd3f25ba57bfaf6838fda139d
Fumio KANEHIRO <fkanehiro@gmail.com>
2017-07-12 10:28:37 +0900

    get actual robot position from hmc

--- .jenkins/getRobotPos.py
+++ .jenkins/getRobotPos.py
@@ -3,9 +3,10 @@ import rtm, OpenHRP
 rtm.nsport = 2809
 rtm.initCORBA()
 
-rh = rtm.findRTC("VirtualRobotHardware0")
-data = rtm.readDataPort(rh.port("waistAbsTransform"))
-pos = data.data.position
-rpy = data.data.orientation
+hmc = rtm.findRTC("hmc")
+data = rtm.readDataPort(hmc.port("pRes"))
+pos = data.data
+data = rtm.readDataPort(hmc.port("rpyRes"))
+rpy = data.data
 print [pos.x,pos.y,pos.z]
 print [rpy.r,rpy.p,rpy.y]
