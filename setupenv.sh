4eb57b95be7415d501a9f34a3375293256714d2e
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2018-01-30 20:51:47 +0900

    Correct typo

--- setupenv.sh
+++ setupenv.sh
@@ -25,12 +25,12 @@ if [ "$DIST_KIND" = "debian" ]; then
     cd ../../
 fi
 
-if [ "$DIST_KIND" = "ubuntu"] && [ "$DIST_VER" = "14.04" ]; then
+if [ "$DIST_KIND" = "ubuntu" ] && [ "$DIST_VER" = "14.04" ]; then
     sudo rm -rf gcc-7.3.0
     wget http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-7.3.0/gcc-7.3.0.tar.gz
     tar zxvf gcc-7.3.0.tar.gz
     cd gcc-7.3.0
-    ./contrib/download_prerequistites
+    ./contrib/download_prerequisites
     mkdir build
     cd build
     ../configure --enable-languages=c,c++ --prefix=$PREFIX --disable-bootstrap --disable-multilib
