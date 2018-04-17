0a1a48b39f83e1160812ca28e056fe1d59ff560e
Fumio KANEHIRO <fkanehiro@gmail.com>
2018-04-17 10:30:22 +0900

    fix bug in choreonoid check

--- task.sh
+++ task.sh
@@ -62,7 +62,7 @@ for ((i=0; i<900; i++)); do
 	break
     fi
     c=`ps -ef | grep choreonoid | grep -v grep | wc -l`
-    if [ $c != 0 ]; then
+    if [ $c  0 ]; then
         echo "choreonoid died"
         exit 1
     fi
@@ -85,7 +85,7 @@ for ((i=0; i<900; i++)); do
 	break
     fi
     c=`ps -ef | grep choreonoid | grep -v grep | wc -l`
-    if [ $c != 0 ]; then
+    if [ $c = 0 ]; then
         echo "choreonoid died"
         exit 1
     fi
@@ -120,7 +120,7 @@ for ((i=0; i<${WAIT}; i++)); do
 	break
     fi
     c=`ps -ef | grep choreonoid | grep -v grep | wc -l`
-    if [ $c != 0 ]; then
+    if [ $c = 0 ]; then
         echo "choreonoid died"
         exit 1
     fi
