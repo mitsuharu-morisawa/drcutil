b0fa6b572b1fa3cef7a202feca01a617c883bfca
Fumio KANEHIRO <fkanehiro@gmail.com>
2018-07-03 11:26:00 +0900

    create summary page from cache

--- .jenkins/printJenkinsResultSummary.py
+++ .jenkins/printJenkinsResultSummary.py
@@ -1,69 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, urllib2, json, base64
+import sys, json, os
 from datetime import datetime
 
-def urlread(url, username, password):
-    request = urllib2.Request(url)
-    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-    request.add_header("Authorization", "Basic %s" % base64string) 
-    return urllib2.urlopen(request)
-
-def printLatestResults(url, username, passowrd, job, n):
+def printLatestResults(job, n):
     print "|[[" + job + "]]|",
-    try:
-        url = url + 'job/' + job + '/api/json?tree=builds[building,duration,number,result,timestamp,url]'
-        r = urlread(url, username, password)
-        root = json.loads(r.read())
-        builds = root['builds']
-    except:
+    cacheFile = job+".json"
+    if not os.path.exists(cacheFile):
         print "|"*n
-    finally:
-        r.close()
+        return
+    f = open(job+".json",'r')
+    results = sorted(json.load(f).items(), key=lambda x: -int(x[0]))
+    f.close()
     for i in range(n):
-        if i < len(builds):
-            result = builds[i]['result']
+        if i < len(results):
+            result = results[i][1]
+            ret = result['result']
             text = ""
-            if result == "SUCCESS":
+            if ret == "SUCCESS":
                 color = "blue"
-            elif result == "UNSTABLE":
-                color = "yellow"
-                try:
-                    r = urlread(builds[i]['url'] + "artifact/task_result.txt", username, password)
-                    line = r.readline()
-                    text = line[0:len(line)-1]
-                except:
-                    pass
-                finally:
-                    r.close()
-            elif result == "FAILURE":
+            elif ret == "FAILURE":
                 color = "red"
-            else:
+            elif ret == "ABORTED":
                 color = "aborted"
-            print "<a href=\""+builds[i]['url']+"artifact/\"><img src=\"https://github.com/isri-aist/hrg-ci-results/wiki/images/"+color+".png\" alt=\""+color+".png\" title=\""+color+".png\"></a>",
+            else:
+                color = "yellow"
+            print "<a href=\""+result['url']+"artifact/\"><img src=\"https://github.com/isri-aist/hrg-ci-results/wiki/images/"+color+".png\" alt=\""+color+".png\" title=\""+color+".png\"></a>"+ret,
         print "|",
     print 
-
-
-if len(sys.argv) > 1:
-    topurl = sys.argv[1]
-    username = sys.argv[2]
-    password = sys.argv[3]
-else:
-    topurl = "http://localhost:8080/"
-    username = "user"
-    passowrd = "passwd"
-
-try:
-    url = topurl + 'api/json?tree=jobs[name,lastStableBuild]'
-    r = urlread(url, username, password)
-    root = json.loads(r.read())
-    jobs = root['jobs']
-except:
-    sys.exit(1)
-finally:
-    r.close()
 
 njob = 10
 
@@ -74,7 +39,7 @@ print "___"
 print ""
 print "|Name|Latest Results"+"|"*njob
 print "|---|"+"--|"*njob
-for job in jobs:
-    if job['name'] != "drcutil" and job['name'] != "drcutil-upload":
-        printLatestResults(topurl, username, password, job['name'], njob)
+for job in sys.argv[1:]:
+    if job != "drcutil" and job != "drcutil-upload":
+        printLatestResults(job, njob)
 print ""
