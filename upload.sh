b160a8af8546bfde4a01aa161477837307dbd910
Fumio KANEHIRO <fkanehiro@gmail.com>
2019-05-21 10:59:48 +0900

    generate artifacts.txt and uploads.txt

--- upload.sh
+++ upload.sh
@@ -13,25 +13,25 @@ upload() {
   if [ -s ${FILENAME} ]; then
       URL=${BUILD_URL}artifact/${FILENAME}
       echo "${LABEL},${FILENAME},${URL}" >> artifacts.txt
-      if [ "$LABEL" = "VIDEO" ]; then
-	  #URL=$(python ${WORKSPACE}/drcutil/.jenkins/upload_video.py --title=${TITLE} --file=${FILENAME} --privacyStatus=unlisted)
-	  :
-      else
-          size=$(stat -c %s $FILENAME)
-          if [ $size -gt 100000000 ]; then
-              URL="too big"
-          else
-	      URL=$(python ${WORKSPACE}/drcutil/.jenkins/remoteBackup.py ${TITLE} ${MIMETYPE} ${FILENAME})
-          fi
-      fi
+      # if [ "$LABEL" = "VIDEO" ]; then
+      # 	  #URL=$(python ${WORKSPACE}/drcutil/.jenkins/upload_video.py --title=${TITLE} --file=${FILENAME} --privacyStatus=unlisted)
+      # 	  :
+      # else
+      #     size=$(stat -c %s $FILENAME)
+      #     if [ $size -gt 100000000 ]; then
+      #         URL="too big"
+      #     else
+      # 	      URL=$(python ${WORKSPACE}/drcutil/.jenkins/remoteBackup.py ${TITLE} ${MIMETYPE} ${FILENAME})
+      #     fi
+      # fi
       echo "${LABEL},${FILENAME},${URL}" >> uploads.txt
   fi
 }
 
-# for file in $(ls *.log); do
-#     upload "BUILD" $file "text/plain"
-# done
-# upload "BUILD" "hmc_log.tar.bz2" "application/x-gzip"
-# upload "BUILD" "SelfCollision.txt" "text/plain"
-# upload "IMAGE" "task.png" "image/png"
-# upload "VIDEO" "task.ogv" "video/ogg"
+for file in $(ls *.log); do
+    upload "BUILD" $file "text/plain"
+done
+upload "BUILD" "hmc_log.tar.bz2" "application/x-gzip"
+upload "BUILD" "SelfCollision.txt" "text/plain"
+upload "IMAGE" "task.png" "image/png"
+upload "VIDEO" "task.ogv" "video/ogg"
