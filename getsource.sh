f7df1f3e0eddcddc2c8041ec9492f26ffda603c3
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2019-07-02 20:22:07 +0900

    Add the possibility to get the source from ssh links

--- getsource.sh
+++ getsource.sh
@@ -2,6 +2,12 @@
 
 source config.sh
 
+if [ $GITHUB_SSH != "1" ]; then
+    GITHUB_LINK=https://github.com/
+else
+    GITHUB_LINK=git@github.com:
+fi
+
 if [ $OSNAME != "Darwin" ]; then
     sudo apt-get -y install git subversion
 fi
@@ -20,7 +26,7 @@ cd $SRC_DIR
 
 get_source_OpenRTM-aist() {
     #get_source "svn co https://svn.openrtm.org/OpenRTM-aist/branches/RELENG_1_1/OpenRTM-aist" OpenRTM-aist
-    get_source "git clone https://github.com/isri-aist/openrtm-aist-cpp.git" OpenRTM-aist
+    get_source "git clone ${GITHUB_LINK}isri-aist/openrtm-aist-cpp.git" OpenRTM-aist
 }
 
 get_source_pcl() {
@@ -28,51 +34,51 @@ get_source_pcl() {
 }
 
 get_source_openhrp3() {
-    get_source "git clone https://github.com/fkanehiro/openhrp3.git" openhrp3
+    get_source "git clone ${GITHUB_LINK}fkanehiro/openhrp3.git" openhrp3
 }
 
 get_source_HRP2() {
-    get_source "git clone https://github.com/isri-aist/hrp2" HRP2
+    get_source "git clone ${GITHUB_LINK}isri-aist/hrp2" HRP2
 }
 
 get_source_HRP2KAI() {
-    get_source "git clone https://github.com/isri-aist/hrp2kai" HRP2KAI
+    get_source "git clone ${GITHUB_LINK}isri-aist/hrp2kai" HRP2KAI
 }
 
 get_source_HRP5P() {
-    get_source "git clone https://github.com/isri-aist/hrp5p" HRP5P
+    get_source "git clone ${GITHUB_LINK}isri-aist/hrp5p" HRP5P
 }
 
 get_source_hrpsys-private() {
-    get_source "git clone https://github.com/isri-aist/hrpsys-private" hrpsys-private
+    get_source "git clone ${GITHUB_LINK}isri-aist/hrpsys-private" hrpsys-private
 }
 
 get_source_hrpsys-state-observation() {
-    get_source "git clone --recursive https://github.com/isri-aist/hrpsys-state-observation" hrpsys-state-observation
+    get_source "git clone --recursive ${GITHUB_LINK}isri-aist/hrpsys-state-observation" hrpsys-state-observation
 }
 
 get_source_hrpsys-base() {
-    get_source "git clone https://github.com/fkanehiro/hrpsys-base" hrpsys-base
+    get_source "git clone ${GITHUB_LINK}fkanehiro/hrpsys-base" hrpsys-base
 }
 
 get_source_state-observation() {
-    get_source "git clone --recursive https://github.com/mehdi-benallegue/state-observation" state-observation
+    get_source "git clone --recursive ${GITHUB_LINK}mehdi-benallegue/state-observation" state-observation
 }
 
 get_source_hmc2() {
-    get_source "git clone https://github.com/jrl-umi3218/hmc2" hmc2
+    get_source "git clone ${GITHUB_LINK}jrl-umi3218/hmc2" hmc2
 }
 
 get_source_hrpsys-humanoid() {
-    get_source "git clone https://github.com/jrl-umi3218/hrpsys-humanoid" hrpsys-humanoid
+    get_source "git clone ${GITHUB_LINK}jrl-umi3218/hrpsys-humanoid" hrpsys-humanoid
 }
 
 get_source_sch-core() {
-    get_source "git clone --recursive https://github.com/mehdi-benallegue/sch-core" sch-core
+    get_source "git clone --recursive ${GITHUB_LINK}mehdi-benallegue/sch-core" sch-core
 }
 
 get_source_savedbg() {
-    get_source "git clone https://github.com/isri-aist/savedbg" savedbg
+    get_source "git clone ${GITHUB_LINK}isri-aist/savedbg" savedbg
     cd savedbg
     git checkout db25d9c9be98f0bce6348bdd425305c799980473
     cd ..
@@ -81,48 +87,48 @@ get_source_savedbg() {
 get_source_octomap() {
     if [ "$DIST_VER" != "16.04" ] && [ "$DIST_VER" != "18.04" ]; then
 	if [ ! -e octomap-$OCTOMAP_VERSION ]; then
-	    wget https://github.com/OctoMap/octomap/archive/v$OCTOMAP_VERSION.tar.gz
+	    wget ${GITHUB_LINK}OctoMap/octomap/archive/v$OCTOMAP_VERSION.tar.gz
 	    tar zxvf v$OCTOMAP_VERSION.tar.gz
 	fi
     fi
 }
 
 get_source_choreonoid() {
-    get_source "git clone https://github.com/isri-aist/choreonoid.git" choreonoid
+    get_source "git clone ${GITHUB_LINK}isri-aist/choreonoid.git" choreonoid
     cd choreonoid/ext
-    get_source "git clone https://github.com/jrl-umi3218/hrpcnoid" hrpcnoid
-    get_source "git clone https://github.com/isri-aist/cnoid-boost-python" cnoid-boost-python
-    get_source "git clone https://github.com/isri-aist/grxui-plugin" grxui-plugin
-    get_source "git clone https://github.com/isri-aist/openhrp-plugin" openhrp-plugin
+    get_source "git clone ${GITHUB_LINK}jrl-umi3218/hrpcnoid" hrpcnoid
+    get_source "git clone ${GITHUB_LINK}isri-aist/cnoid-boost-python" cnoid-boost-python
+    get_source "git clone ${GITHUB_LINK}isri-aist/grxui-plugin" grxui-plugin
+    get_source "git clone ${GITHUB_LINK}isri-aist/openhrp-plugin" openhrp-plugin
     cd ../..
 }
 
 get_source_trap-fpe() {
     if [ ! -e DynamoRIO-$DYNAMORIO_VERSION.tar.gz ]; then
-	wget https://github.com/DynamoRIO/dynamorio/releases/download/$DYNAMORIO_RELEASE/DynamoRIO-$DYNAMORIO_VERSION.tar.gz
+	wget ${GITHUB_LINK}DynamoRIO/dynamorio/releases/download/$DYNAMORIO_RELEASE/DynamoRIO-$DYNAMORIO_VERSION.tar.gz
     fi
     get_source "git clone https://bitbucket.org/jun0-aist/trap-fpe" trap-fpe
 }
 
 get_source_flexiport() {
-    get_source "git clone https://github.com/gbiggs/flexiport" flexiport
+    get_source "git clone ${GITHUB_LINK}gbiggs/flexiport" flexiport
 }
 
 get_source_hokuyoaist() {
-    get_source "git clone https://github.com/fkanehiro/hokuyoaist" hokuyoaist
+    get_source "git clone ${GITHUB_LINK}fkanehiro/hokuyoaist" hokuyoaist
 }
 
 get_source_rtchokuyoaist() {
-    get_source "git clone https://github.com/fkanehiro/rtchokuyoaist" rtchokuyoaist
+    get_source "git clone ${GITHUB_LINK}fkanehiro/rtchokuyoaist" rtchokuyoaist
 }
 
 get_source_is-jaxa() {
-    get_source "git clone https://github.com/isri-aist/is-jaxa" is-jaxa
+    get_source "git clone ${GITHUB_LINK}isri-aist/is-jaxa" is-jaxa
 }
 
 get_source_takenaka() {
     cd choreonoid/ext
-    get_source "git clone https://github.com/isri-aist/takenaka" takenaka
+    get_source "git clone ${GITHUB_LINK}isri-aist/takenaka" takenaka
     cd ../..
 }
 
