b9d3ea0bd139d3ecf6456342bc2a5546e838d15f
Fumio KANEHIRO <fkanehiro@gmail.com>
2020-09-17 13:47:45 +0900

    continue installation even if apt-get returns an error

--- setupenv.sh
+++ setupenv.sh
@@ -62,7 +62,7 @@ setupenv_hrpsys-base() {
     if [ $OSNAME = "Darwin" ]; then
 	brew install opencv sdl boost-python
     else
-	sudo apt-get -y --force-yes install libxml2-dev libsdl-dev libglew-dev libopencv-dev libqhull-dev freeglut3-dev libxmu-dev python-dev libboost-python-dev ipython openrtm-aist-python
+	sudo apt-get -y --force-yes install libxml2-dev libsdl-dev libglew-dev libopencv-dev libqhull-dev freeglut3-dev libxmu-dev python-dev libboost-python-dev ipython openrtm-aist-python || true
 	if [ "$DIST_KIND" = "ubuntu" ] && [ "$DIST_VER" != "18.04" ]; then
             sudo apt-get -y --force-yes install libcvaux-dev libhighgui-dev
 	fi
